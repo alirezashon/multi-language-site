a2e5e162cbe737c761ab692d15975b69add06c56
alirezashon <alirezafeshki2017@gmail.com>
2024-06-08 18:26:17 +0330

    first deploy

--- src/Layouts/index.tsx
+++ src/Layouts/index.tsx
@@ -3,31 +3,12 @@ import Footer from "@/Components/Navigation/Footer"
 import { LanguageProvider } from "../Context"
 import { FaWhatsapp } from "react-icons/fa"
 import styles from "./index.module.css"
-import { useEffect, useState } from "react"
 
 const Layout = ({ children }: any) => {
-  const [navishen, setNavishen] = useState<"fixed" | "absolute">("absolute")
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > window.innerHeight / 2) {
-        setNavishen("fixed")
-      } else if (window.scrollY > 100) {
-        setNavishen("absolute")
-      }
-    }
-
-    window.addEventListener("scroll", handleScroll)
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll)
-    }
-  }, [])
   return (
     <LanguageProvider>
       <div>
-        <div style={{ position: navishen ,background:'red'}}>
-          <Navigation />
-        </div>
+        <Navigation />
         <main>{children}</main>
         <Footer />
         <FaWhatsapp
